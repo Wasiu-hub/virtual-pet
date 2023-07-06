d5a6ff7c7136de3a8cfadc6fe20f7a0293e1653e
Wasiu-hub <contactbow@yahoo.com>
2023-07-06 01:48:54 +0100

    pet named

---
+++ --test--/pet.test.js
@@ -0,0 +1,8 @@
+const Pet = require("../src/pet");
+
+describe("constructor", () => {
+  it("sets the name property", () => {
+    const pet = new Pet("Fido");
+    expect(pet.name).toEqual("Fido");
+  });
+});
